dfba2b4fb17b258f6488d069e0552b100dc098f9
Alejandro Guevara <alejandroguevara@alejandros-mbp.speedport.ip>
2024-09-01 13:10:33 +0200

    First commit

---
+++ src/products/products.service.ts
@@ -0,0 +1,90 @@
+import {
+  Injectable,
+  Logger,
+  NotFoundException,
+  OnModuleInit,
+} from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { PrismaClient } from '@prisma/client';
+import { PaginationDto } from 'src/common';
+
+@Injectable()
+export class ProductsService extends PrismaClient implements OnModuleInit {
+  private readonly logger = new Logger('ProductsService');
+
+  onModuleInit() {
+    this.$connect();
+    this.logger.log('Connected to database');
+  }
+  create(createProductDto: CreateProductDto) {
+    return this.product.create({
+      data: createProductDto,
+    });
+  }
+
+  async findAll(paginationDto: PaginationDto) {
+    const { page, limit } = paginationDto;
+    const total = await this.product.count({ where: { available: true } });
+    const totalPages = Math.ceil(total / limit);
+    return {
+      data: await this.product.findMany({
+        skip: (page - 1) * limit,
+        take: limit,
+        where: { available: true },
+      }),
+      metadata: {
+        total,
+        limit,
+        page,
+        totalPages,
+      },
+    };
+  }
+
+  async findOne(id: number) {
+    const product = await this.product.findUnique({
+      where: {
+        id,
+        available: true,
+      },
+    });
+    if (!product) {
+      throw new NotFoundException(`Product with id ${id} not found`);
+    }
+    return product;
+  }
+
+  async update(id: number, updateProductDto: UpdateProductDto) {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { id: __, ...data } = updateProductDto;
+
+    await this.findOne(id);
+    return this.product.update({
+      where: {
+        id,
+      },
+      data: data,
+    });
+  }
+
+  async remove(id: number) {
+    await this.findOne(id);
+    // Hard delete
+    // return this.product.delete({
+    //   where: {
+    //     id,
+    //   },
+    // });
+
+    // Soft delete
+    return this.product.update({
+      where: {
+        id,
+      },
+      data: {
+        available: false,
+      },
+    });
+  }
+}
